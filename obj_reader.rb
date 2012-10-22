eb78897549d1b6086218cd3cae353c1552dfb05c
MaratVmk <maratvmk@mail.ru>
2012-10-23 03:18:01 +0400

    refactor first commits

--- obj_reader.rb
+++ obj_reader.rb
@@ -3,6 +3,18 @@ require_relative "face.rb"
 
 v = Array(0); f_obj = []; e = []; faces = []
 
+init_edges = lambda do |k, n, f|
+	unless i = e.index(Edge.new(b: f[k], e: f[n]))
+		e << Edge.new(b: f[k], e: f[n], l: f_obj.index(f)) 
+		e << Edge.new(b: f[n], e: f[k], r: f_obj.index(f))
+		e.size - 2
+	else
+		e[i].set(r: f_obj.index(f))
+		e[i+1].set(l: f_obj.index(f))
+		i + 1
+	end	
+end
+
 File.readlines("obj/cube.obj").each do |l|
 	if l[0] == 'v' and l[1] == ' '
 		v << l[2..6].split(' ').map { |e| e.to_f }
@@ -12,36 +24,16 @@ File.readlines("obj/cube.obj").each do |l|
 end
 
 f_obj.each do |f|
-	unless i = e.index(Edge.new(b: f[0], e: f[1]))
-		ab = e.size
-		e << Edge.new(b: f[0], e: f[1], l: f_obj.index(f)) 
-		e << Edge.new(b: f[1], e: f[0], r: f_obj.index(f))
-	else
-		ab = i + 1
-		e[i].set(r: f_obj.index(f))
-		e[i+1].set(l: f_obj.index(f))
-	end
-
-	unless i = e.index(Edge.new(b: f[1], e: f[2]))
-		bc = e.size
-		e << Edge.new(b: f[1], e: f[2], l: f_obj.index(f)) 
-		e << Edge.new(b: f[2], e: f[1], r: f_obj.index(f))
-	else
-		bc = i + 1 
-		e[i].set(r: f_obj.index(f))
-		e[i+1].set(l: f_obj.index(f))
-	end
-
-	unless i = e.index(Edge.new(b: f[2], e: f[0]))
-		ca = e.size
-		e << Edge.new(b: f[2], e: f[0], l: f_obj.index(f)) 
-		e << Edge.new(b: f[0], e: f[2], r: f_obj.index(f))
-	else
-		ca = i + 1
-		e[i].set(r: f_obj.index(f))
-		e[i+1].set(l: f_obj.index(f))
-	end
+	ab = init_edges.call(0, 1, f)
+	bc = init_edges.call(1, 2, f)
+	ca = init_edges.call(2, 0, f)
 
 	faces << Face.new(a: f[0], b: f[1], c: f[2], ab: ab, bc: bc, ca: ca)
 end
 
+e.each { |e| p e }
+puts 
+faces.each { |e| p e }
+puts e.size
+puts faces.size
+p e[0]
