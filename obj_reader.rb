4b9efa3070f1a98ed7e6239317a739a925beca8b
MaratVmk <maratvmk@mail.ru>
2012-10-23 05:40:29 +0400

    refactored full initialize

--- obj_reader.rb
+++ obj_reader.rb
@@ -4,22 +4,21 @@ require_relative "face.rb"
 v = Array(0); f_obj = []; e = []; faces = []
 
 File.readlines("obj/cube.obj").each do |l|
-	if l[0] == 'v' and l[1] == ' '
-		v << l[2..6].split(' ').map { |e| e.to_f }
-	elsif l[0] == 'f' and l[1] == ' '
-	   f_obj << l[2..6].split(' ').map { |e| e.to_i }
+	case l[0..1]
+		when 'v ' then v << l[2..6].split(' ').map { |e| e.to_f }
+		when 'f ' then f_obj << l[2..6].split(' ').map { |e| e.to_i }
 	end
 end
 
 init_edges = lambda do |k, n, f|
-	index = f_obj.index f
+	ind = f_obj.index(f)
 	unless i = e.index(Edge.new(b: f[k], e: f[n]))
-		e << Edge.new(b: f[k], e: f[n], l: index)
-		e << Edge.new(b: f[n], e: f[k], r: index)
+		e << Edge.new(b: f[k], e: f[n], l: ind)
+		e << Edge.new(b: f[n], e: f[k], r: ind)
 		e.size - 2
 	else
-		e[i-1].set(r: index)
-		e[i].set(l: index)
+		e[i-1].set(r: ind)
+		e[i].set(l: ind)
 		i
 	end	
 end
@@ -33,16 +32,16 @@ f_obj.each do |f|
 end
 
 e.each do |ed|
+	l_face = faces[ed.l]; r_face = faces[ed.r]
 	case e.index(ed) 
-		when faces[ed.l].ab then ed.e_next = faces[ed.l].bc
-		when faces[ed.l].bc then ed.e_next = faces[ed.l].ca
-		when faces[ed.l].ca then ed.e_next = faces[ed.l].ab
+		when l_face.ab then ed.e_next = l_face.bc
+		when l_face.bc then ed.e_next = l_face.ca
+		when l_face.ca then ed.e_next = l_face.ab
 	end
-	case i = e.index(ed.inverse) 
-		when faces[ed.r].ab then ed.b_next = faces[ed.r].bc
-		when faces[ed.r].bc then ed.b_next = faces[ed.r].ca
-		when faces[ed.r].ca then ed.b_next = faces[ed.r].ab
-		else puts i
+	case e.index(ed.inverse) 
+		when r_face.ab then ed.b_next = r_face.bc
+		when r_face.bc then ed.b_next = r_face.ca
+		when r_face.ca then ed.b_next = r_face.ab
 	end				
 end
 
