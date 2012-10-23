2d273e6b0155ac9d215a305852dad92866ee7f27
MaratVmk <maratvmk@mail.ru>
2012-10-23 06:05:55 +0400

    a obj_reader is ready

--- obj_reader.rb
+++ obj_reader.rb
@@ -3,13 +3,6 @@ require_relative "face.rb"
 
 v = Array(0); f_obj = []; e = []; faces = []
 
-File.readlines("obj/cube.obj").each do |l|
-	case l[0..1]
-		when 'v ' then v << l[2..6].split(' ').map { |e| e.to_f }
-		when 'f ' then f_obj << l[2..6].split(' ').map { |e| e.to_i }
-	end
-end
-
 init_edges = lambda do |k, n, f|
 	ind = f_obj.index(f)
 	unless i = e.index(Edge.new(b: f[k], e: f[n]))
@@ -23,29 +16,37 @@ init_edges = lambda do |k, n, f|
 	end	
 end
 
-f_obj.each do |f|
-	ab = init_edges.call(0, 1, f)
-	bc = init_edges.call(1, 2, f)
-	ca = init_edges.call(2, 0, f)
-	
-	faces << Face.new(a: f[0], b: f[1], c: f[2], ab: ab, bc: bc, ca: ca)
-end
-
-e.each do |ed|
-	l_face = faces[ed.l]; r_face = faces[ed.r]
-	case e.index(ed) 
-		when l_face.ab then ed.e_next = l_face.bc
-		when l_face.bc then ed.e_next = l_face.ca
-		when l_face.ca then ed.e_next = l_face.ab
+init = lambda do
+	f_obj.each do |f|
+		ab = init_edges.call(0, 1, f)
+		bc = init_edges.call(1, 2, f)
+		ca = init_edges.call(2, 0, f)
+		
+		faces << Face.new(a: f[0], b: f[1], c: f[2], ab: ab, bc: bc, ca: ca)
 	end
-	case e.index(ed.inverse) 
-		when r_face.ab then ed.b_next = r_face.bc
-		when r_face.bc then ed.b_next = r_face.ca
-		when r_face.ca then ed.b_next = r_face.ab
-	end				
+
+	e.each do |ed|
+		l_face = faces[ed.l]; r_face = faces[ed.r]
+		case e.index(ed) 
+			when l_face.ab then ed.e_next = l_face.bc
+			when l_face.bc then ed.e_next = l_face.ca
+			when l_face.ca then ed.e_next = l_face.ab
+		end
+		case e.index(ed.inverse) 
+			when r_face.ab then ed.b_next = r_face.bc
+			when r_face.bc then ed.b_next = r_face.ca
+			when r_face.ca then ed.b_next = r_face.ab
+		end				
+	end
+	[v, e, faces]
 end
 
-
-e.each { |e| p e  }
-puts 
-faces.each { |e|  p e }
+read_init = lambda do |file|
+	File.readlines(file).each do |l|
+		case l[0..1]
+			when 'v ' then v << l[2..6].split(' ').map { |e| e.to_f }
+			when 'f ' then f_obj << l[2..6].split(' ').map { |e| e.to_i }
+		end
+	end
+	init.call
+end
