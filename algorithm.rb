b3c83a743ea8c2af3296115af26077bc925c0996
MaratVmk <maratvmk@mail.ru>
2012-11-14 06:48:17 +0400

    update

--- algorithm.rb
+++ algorithm.rb
@@ -8,13 +8,15 @@ stack = []
 
 f_flag = Array.new(faces.size) { |f| f = false } #грань обработан или нет
 is_cntr = Array.new(e.size) { |c| c = false }    #ребро контурный или нет
+cntr_cycles = []
+cntr_cycle_borders = [0]
 
 for ind in 0..faces.size-1
-	if !f_flag[ind] && faces[ind].facial(lt)  #гран не обработан и лицевой
+	if !f_flag[ind] && faces[ind].facial(lt)  #грань не обработан и лицевой
 		stack.push ind 		
-		while ind = stack.pop #пока стек не пуст
-			f = faces[ind]
-			f_flag[ind] = true
+		while id = stack.pop #пока стек не пуст
+			f = faces[id]
+			f_flag[id] = true
 			for i in 0..2 #рассмотрим все соседние грани
 				curr_f = faces[e[f[i]].r]
 				unless f_flag[j = faces.index(curr_f)] #не обработан
@@ -26,11 +28,17 @@ for ind in 0..faces.size-1
 				end
 			end
 		end
+		for i in 0..e.size-1
+			if is_cntr[i]
+				cntr = i
+				break
+			end 
+		end		
 	end
 end
 
-for i in 1..e.size-1
-	if is_cntr[i] == true
+for i in 0..e.size-1
+	if is_cntr[i]
 		p e[i]
 	end
 end
