e9683e37bde27b3b3ccf66a0d7b9989423abce00
MaratVmk <maratvmk@mail.ru>
2012-11-19 15:58:22 +0400

    add io folder

---
+++ io/obj_triangulate.rb
@@ -0,0 +1,17 @@
+f = File.new "../obj/t_"+ARGV[0], "w"
+File.readlines("../obj/"+ARGV[0]).each do |l|
+	case l[0..1]
+		when 'f '
+			tmp = l[2..-1].split(' ').map!{ |e| e.split('/') }
+		   if tmp.size == 4
+		   	f.write "f #{tmp[0][0]} #{tmp[1][0]} #{tmp[2][0]}\n" 
+		   	f.write "f #{tmp[2][0]} #{tmp[3][0]} #{tmp[0][0]}\n"
+		   elsif tmp.size == 3
+		   	f.write "f #{tmp[0][0]} #{tmp[1][0]} #{tmp[2][0]}\n" 
+		   else
+		   	puts 'Error'
+		   end
+		else
+			f.write l
+	end
+end
