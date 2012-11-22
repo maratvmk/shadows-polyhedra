271466cd20050d6e83005f22af633b3137cfd5e6
MaratVmk <maratvmk@mail.ru>
2012-11-22 19:29:01 +0400

    self_intercetions is ready

--- logics/self_intersections.rb
+++ logics/self_intersections.rb
@@ -1,6 +1,6 @@
 def remove_intersections polygons, asm_prs
 	for i in 0..polygons.size-1
-		p = polygons[i]; psize = p.size; e = []; asm_eds = []; cross = {}
+		p = polygons[i]; psize = p.size; e = []; asm_eds = []; cross = {index: []}
 		for j in 0..psize-1
 			e << Edge.new( b: (j-1) % psize,  e: j)
 			asm_eds << j if asm_prs[i].include? p[j]
@@ -13,8 +13,13 @@ def remove_intersections polygons, asm_prs
 				for m in mass
 					puts m
 					if cr = e[asm].intersect(e[m], p)
-						cross[m] = [cr, asm]
-						cross[asm] = [cr, m]
+						if asm > m
+							cross[m] = [cr, asm]
+							cross[:index] += (m..asm-1).to_a
+						else
+						 	cross[asm] = [cr, m]
+						 	cross[:index] += (asm..m-1).to_a
+						end
 						flag = true
 						break
 					end
@@ -22,6 +27,19 @@ def remove_intersections polygons, asm_prs
 				asm = (asm-1) % psize
 			end
 		end
-		p cross
+		p cross[:index]
+		for j in 0..psize-1
+			if cross[:index].include?(j)
+				if cross[j]
+					p[e[j].e] = cross[j][0]
+					p[e[cross[j][1]].b] = cross[j][0]
+				else
+					e[j] = nil
+					p[j] = nil
+				end
+			end
+		end
+		e.compact!; p.compact!
 	end
+	polygons
 end
