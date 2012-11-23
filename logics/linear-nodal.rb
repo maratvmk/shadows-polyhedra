c8df9fa549b95e17b4dd009a2a4ee6e4db8f471f
MaratVmk <maratvmk@mail.ru>
2012-11-23 19:26:39 +0400

    linear-nodal model is builded

--- logics/linear-nodal.rb
+++ logics/linear-nodal.rb
@@ -1,30 +1,54 @@
 require_relative "../models/vec3f.rb"
 require_relative "../models/edge.rb"
 
-a = Vec3f.new 0, 0, 0
-b = Vec3f.new 3, 0, 0
-c = Vec3f.new 2, 4, 0
+a = Vec3f.new 0.0, 0.0, 0.0
+b = Vec3f.new 3.0, 0.0, 0.0
+c = Vec3f.new 2.0, 4.0, 0.0
 
-d = Vec3f.new 1, 3, 0
-e = Vec3f.new 4, -4, 0
-f = Vec3f.new 5, 3, 0
+d = Vec3f.new 1.0, 3.0, 0.0
+e = Vec3f.new 4.0, -4.0, 0.0
+f = Vec3f.new 5.0, 3.0, 0.0
 
 p1 = [a, b, c]
 p2 = [d, e, f]
-#p1.sort!{|x,y| x.x <=> y.x}
-#p2.sort!{|x,y| x.x <=> y.x}
-e = []; @l = []
+
+e = []; @l = []; cr = []
 v = (p1 + p2).uniq{|e| [e.x, e.y]}
 
 def init p
 	offset = @l.size
 	for i in 0..p.size-1
-		@l << Edge.new(b: i + offset, e: (i+1)%p.size + offset, length: (p[i]-p[(i+1)%p.size]).length)
+		@l << Edge.new(b: i + offset, e: (i+1) % p.size + offset, length: (p[i] - p[(i+1) % p.size]).length)
 	end
 end
 
-init(p1); init(p2)
-
+init p1; init p2
 @l.sort!{|a,b| a.length <=> b.length}
 
-@l.each { |e| p e }
+while ed = @l.pop
+	for i in 0..e.size-1
+		if !ed.incident(e[i]) and tmp = ed.intersect(e[i], v)
+			v << tmp; cr << tmp
+			e << Edge.new(b: e[i].b, e: v.size-1)
+			e << Edge.new(b: v.size-1, e: e[i].e)
+			e[i] = nil
+		end
+	end
+	e.compact! #v.uniq!{|e| [e.x, e.y]}
+
+	cr.sort!{|a,b| (v[ed.b] - a).length <=> (v[ed.b] - b).length}
+	
+	for i in 0..cr.size-2
+		e << Edge.new(b: v.index(cr[i]), e: v.index(cr[i+1]))
+	end
+
+	if cr.empty? 
+		e << ed
+	else 
+		e << Edge.new(b: ed.b, e: v.index(cr[0]))
+		e << Edge.new(b: v.index(cr[-1]), e: ed.e)
+		cr.clear
+	end
+end
+
+e.each { |e| p e }
